53407bec8f41b32448bdfe11fa87914f88d38524
Emmanuel Viana <oliveiravianaviana@gmail.com>
2020-10-16 13:27:23 -0300

    modificando database

--- src/database/fakedata.js
+++ src/database/fakedata.js
@@ -2,10 +2,14 @@ module.exports [
     {
         id: 1,
         name: 'Lar das meninas',
-        description: 
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+
+        ]
     },
     {
-        id: 2
-
+        id: 2,
+        name: 'Lar das meninas',
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social."
     }
 ]
