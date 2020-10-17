da507e1adba6f904d9bf65de31e35506139e28ee
Emmanuel Viana <oliveiravianaviana@gmail.com>
2020-10-16 21:45:57 -0300

    sqlite

--- src/database/fakedata.js
+++ src/database/fakedata.js
@@ -3,7 +3,7 @@ module.exports [
         id: 1,
         lat: "-17.5420368",
         long: "-39.7463134",
-        name: 'Lar das meninas',
+        name: "Lar das meninas",
         description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
         images: [ 
             "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpsiconlinews.com%2F2017%2F06%2Fas-criancas-precisam-ser-felizes-nao-de-serem-as-melhores.html&psig=AOvVaw33pJuiCNvWy0uHtQsQPQa3&ust=1602962363995000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJjRsaXquewCFQAAAAAdAAAAABAD"
@@ -21,7 +21,7 @@ module.exports [
         id: 1,
         lat: "-17.5420368",
         long: "-39.7453134",
-        name: 'Lar dos meninos',
+        name: "Lar das meninos",
         description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
         images: [ 
             "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpsiconlinews.com%2F2017%2F06%2Fas-criancas-precisam-ser-felizes-nao-de-serem-as-melhores.html&psig=AOvVaw33pJuiCNvWy0uHtQsQPQa3&ust=1602962363995000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJjRsaXquewCFQAAAAAdAAAAABAD"
