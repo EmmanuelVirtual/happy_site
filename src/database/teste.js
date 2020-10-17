b2852eec7dbdfbb50e73bf36bcbacde790a05aa8
Emmanuel Viana <oliveiravianaviana@gmail.com>
2020-10-16 21:47:27 -0300

    sqlite

--- src/database/teste.js
+++ src/database/teste.js
@@ -2,4 +2,22 @@ const Database = require ('./db');
 
 Database.then(function(db){
     //inserir dados na tabela
+        db.run(`
+            INSERT INTO orphanages (
+                lat,
+                lng,
+                name,
+                whatsapp,
+                images,
+                instructions,
+                opening_hours,
+                open_on_wekeends
+            ) VALUES (
+                "-17.5420368"
+                "-39.7463134"
+                "Lar das meninas"
+            );
+        `)
+
+    //consultar dados da tabela
 })
